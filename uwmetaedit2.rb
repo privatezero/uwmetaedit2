b30b69fd2ff2bbae642e4bc139e9c93a91e6f477
weaver <theandrewjw@gmail.com>
2019-10-24 13:07:49 -0700

    upgrayded

--- uwmetaedit2.rb
+++ uwmetaedit2.rb
@@ -5,11 +5,11 @@ require 'yaml'
 require 'mediainfo'
 require 'pry'
 
-# Check for windows
+# Check for $windows
 if Gem.win_platform?
-  Windows = true
+  $windows = true
 else
-  Windows = false
+  $windows = false
 end
 
 #Set up/Load config
@@ -28,8 +28,8 @@ end
  configOptions = YAML.load(File.read(configPath))
 
 def getOutputDir()
-  if Windows
-    targetFile = `powershell "Add-Type -AssemblyName System.windows.forms|Out-Null;$f=New-Object System.Windows.Forms.FolderBrowserDialog;$f.SelectedPath = 'C:\';$f.Description = 'Select Output Directory';$f.ShowDialog((New-Object System.Windows.Forms.Form -Property @{TopMost = $true }))|Out-Null;$f.SelectedPath"`.strip + '\\'
+  if $windows
+    targetFile = `powershell "Add-Type -AssemblyName System.$windows.forms|Out-Null;$f=New-Object System.$windows.Forms.FolderBrowserDialog;$f.SelectedPath = 'C:\';$f.Description = 'Select Output Directory';$f.ShowDialog((New-Object System.$windows.Forms.Form -Property @{TopMost = $true }))|Out-Null;$f.SelectedPath"`.strip + '\\'
   else
     targetFile = `zenity --file-selection`.strip
   end
@@ -47,7 +47,7 @@ def embedBext(targetFile, origin, codeHist1, codeHist2, collNumber, itemNumber)
   command << 'bwfmetaedit' 
   command << '--reject-overwrite'
   command << "--Originator=#{origin}"
-  command << "--Description=#{collNumber}"
+  command << "--Description=Collection number: #{collNumber}, Item number #{itemNumber}"
   command << "--OriginatorReference=#{File.basename(targetFile)}"
   command << "--History=#{history}"
   command << "--IARL=#{origin}"
@@ -55,7 +55,11 @@ def embedBext(targetFile, origin, codeHist1, codeHist2, collNumber, itemNumber)
   command << "--OriginationTime=#{modtime}"
   command << '--MD5-Embed'
   command << "#{targetFile}"
-  system(*command)
+  if system(*command) && Gui
+    $window.alert("Embedding done")
+  elsif Gui
+    $window.alert("Error occurred - please double check file and settings")
+  end
 end
 
 
@@ -65,28 +69,33 @@ history1 = configOptions['history1']
 history2 = configOptions['history2']
 collection = configOptions['collection']
 
-if ARGV.length.positive?
-  puts 'HELLO WORLD'
-else
-  window = Flammarion::Engraving.new
-  window.image("")
-  window.title("Welcome to UW Metaedit 2.0")
-  window.pane("BEXT").puts("BEXT Info", replace:true)
-  origin = window.pane("BEXT").input('Originator', options = {value:originator})
-  codeHist1 = window.pane("BEXT").input('Encoding History Line 1' , options = {value:history1})
-  codeHist2 = window.pane("BEXT").input('Encoding History Line 2', options = {value:history2})
-  window.pane("Items").orientation = :horizontal
-  window.pane("Items").puts("Item Info", replace:true)
-  collNumber = window.pane("Items").input('Collection Number(s)', options = {value:collection})
-  itemNumber = window.pane("Items").input('Item Number')
-  window.pane("Items").button("Save Settings") {
+unless ARGV.length.positive?
+  Gui = true
+  $window = Flammarion::Engraving.new
+  $window.image("")
+  $window.title("Welcome to UW Metaedit 2.0")
+  $window.pane("Items").orientation = :horizontal
+  $window.pane("Items").puts("Item Info", replace:true)
+  collNumber = $window.pane("Items").input('Collection Number(s)', options = {value:collection})
+  itemNumber = $window.pane("Items").input('Item Number')
+  $window.pane("Items").button("Save Settings") {
     configOptions['originator'] = origin.to_s
     configOptions['history1'] = codeHist1.to_s
     configOptions['history2'] = codeHist2.to_s
     configOptions['collection'] = collNumber.to_s
     File.open(configPath, "w") { |file| file.write(configOptions.to_yaml) }
    }
-  targetFile = window.pane("Items").button('Select Target') { targetFile = getOutputDir() }
-  window.pane("Items").button('Embed Metadata') { embedBext(targetFile, origin, codeHist1, codeHist2, collNumber, itemNumber) }
-  window.wait_until_closed
+  targetFile = $window.pane("Items").button('Select Target') { targetFile = getOutputDir() }
+  $window.pane("Items").button('Embed Metadata') { embedBext(targetFile, origin, codeHist1, codeHist2, collNumber, itemNumber) }
+  $window.pane("BEXT").puts("BEXT Info", replace:true)
+  origin = $window.pane("BEXT").input('Originator', options = {value:originator})
+  codeHist1 = $window.pane("BEXT").input('Encoding History Line 1' , options = {value:history1})
+  codeHist2 = $window.pane("BEXT").input('Encoding History Line 2', options = {value:history2})
+  $window.wait_until_closed
+else
+    Gui = false
+    targetFile = ARGV[0]
+    itemNumber = ARGV[1]
+    binding.pry
+    embedBext(targetFile, originator, history1, history2, collection, itemNumber)
 end
